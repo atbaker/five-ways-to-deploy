e5587c01471a61743769e349c87f7f2d8ee9a891
Andrew T. Baker <abaker@twilio.com>
2017-05-19 13:13:57 -0700

    Tweaking

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
